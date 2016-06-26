15ba42a69183a3e5902e702003af2c347def9b77
David Negstad <davidn@pk4media.com>
2016-06-25 20:11:28 -0700

    Module with namespace

--- index.d.ts
+++ index.d.ts
@@ -7,32 +7,32 @@
  * Allows for triggering Seneca actions based on express request properties
  */
 
-import express = require("express");
-import seneca = require("seneca");
-
-declare namespace pliny {
-  interface IMessage {
-    req$?: express.Request;
-    res$?: express.Response;
-  }
-
-  interface IRouteOptions {
-      pin?: string | {
-          [key: string]: any;
-      };
-      query?: string[] | boolean;
-      body?: string[] | boolean;
-      params?: string[] | boolean;
-      response?: (req: express.Request, res: express.Response, err: any, message: IMessage) => void;
-      handler?: <T>(req: express.Request, res: express.Response, message: IMessage, action: any, respond: (err: any, message: T) => void) => void;
-  }
-
-  export interface IRouteExport {
-    (options: IRouteOptions): void;
-  }
-}
-
 declare module "pliny" {
+  import express = require("express");
+  import seneca = require("seneca");
+
+  namespace pliny {
+    export interface IMessage {
+      req$?: express.Request;
+      res$?: express.Response;
+    }
+
+    export interface IRouteOptions {
+        pin?: string | {
+            [key: string]: any;
+        };
+        query?: string[] | boolean;
+        body?: string[] | boolean;
+        params?: string[] | boolean;
+        response?: (req: express.Request, res: express.Response, err: any, message: IMessage) => void;
+        handler?: <T>(req: express.Request, res: express.Response, message: IMessage, action: any, respond: (err: any, message: T) => void) => void;
+    }
+
+    export interface IRouteExport {
+      (options: IRouteOptions): void;
+    }
+  }
+
   var pliny: seneca.IPlugin<pliny.IRouteOptions>;
   export = pliny;
 }
